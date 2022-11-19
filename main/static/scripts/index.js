122bb7ebb925a2c831f17b3af9e319e5cbbb38c1
Prateek Agrawal <20bcs101@iiitdwd.ac.in>
2022-11-19 14:36:13 +0530

    Add files via upload

---
+++ main/static/scripts/index.js
@@ -0,0 +1,128 @@
+
+const CAMERA_SPEED = 2000;
+const CAMERA_ALTITUDE = .8
+
+
+function scroll(){
+    var objDiv = document.getElementById("terminal");
+    objDiv.scrollTop = objDiv.scrollHeight;
+}
+
+
+function tracerouteGlobe(pos) {
+    // remove previous data
+    globe.arcsData([]);
+    let labelsData = [];
+    $('#terminal').empty();
+    // add home
+    labelsData.push({
+        latitude: pos.coords.latitude,
+        longitude: pos.coords.longitude,
+        name: 'Home',
+        info: {city: 'Home'}
+    })
+    globe.labelsData(labelsData);
+    globe.pointOfView({lat: pos.coords.latitude, lng: pos.coords.longitude, altitude: CAMERA_ALTITUDE }, CAMERA_SPEED);
+
+
+    // var currentHost = window.location.host;
+    // console.log(currentHost);
+    // var socket = new WebSocket(`wss://${currentHost}/ws`);
+
+    // socket.onopen = function(e) {
+    //     let host = document.getElementById('host').value;
+    //     socket.send(`{"data": {
+    //         "host": "${host}",
+    //         "token": "${API_TOKEN}"
+    //     }}`);
+    // };
+    
+    socket.onmessage = function(event) {
+        let data = JSON.parse(event.data);
+        console.log("Received Message");
+        // print message to terminal output on page
+        // if (data.type === "output" || data.type === "input") {
+        //     let output = `<p class=" terminal--${data.type} ">${data.msg}</p>`;
+        //     $('#terminal').append(output);
+        //     scroll();
+        // // it's a geo message
+        // } else {
+        //     if (!data.msg.bogon) {
+        //         let arcsData = globe.arcsData();
+        //         let labelsData = globe.labelsData();
+        //         let last_location = labelsData.at(-1);
+        //         let new_loc_lat = parseFloat(data.msg.loc.split(",")[0]);
+        //         let new_loc_lng = parseFloat(data.msg.loc.split(",")[1]);
+        //         arcsData.push({
+        //             startLat: last_location.latitude,
+        //             startLng: last_location.longitude,
+        //             endLat: new_loc_lat,
+        //             endLng: new_loc_lng,
+        //             color: 'white',
+        //             name: `${last_location.name} -> ${data.msg.city}`
+        //         });
+        //         labelsData.push({
+        //             latitude: new_loc_lat,
+        //             longitude: new_loc_lng,
+        //             name: data.msg.city,
+        //             info: data.msg
+        //         })
+        //         globe.arcsData(arcsData);
+        //         globe.labelsData(labelsData);
+        //         globe.pointOfView({lat: new_loc_lat, lng: new_loc_lng, altitude: CAMERA_ALTITUDE }, CAMERA_SPEED);
+        //     }
+        // }
+    };
+
+    socket.onerror = function(event) {
+        console.log(event);
+    };
+
+}
+
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+function main() {
+    let options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    };
+    navigator.geolocation.getCurrentPosition(tracerouteGlobe, error, options);
+}
+
+
+// prompt user for api token
+const API_TOKEN = token;
+const globe = Globe()
+    .globeImageUrl("https://unpkg.com/three-globe@2.18.11/example/img/earth-blue-marble.jpg")
+    .bumpImageUrl("https://unpkg.com/three-globe/example/img/earth-topology.png")
+    .backgroundImageUrl("https://unpkg.com/three-globe@2.18.11/example/img/night-sky.png")
+    .arcColor('color')
+    .arcLabel(d => null)
+    .arcDashLength(0.1)
+    .arcDashGap(1)
+    .arcDashAnimateTime(1000)
+    .arcsTransitionDuration(100)
+    .labelLat(d => d.latitude)
+    .labelLng(d => d.longitude)
+    .labelText(d => d.info.city)
+    .labelDotRadius(d => .1)
+    .labelColor(() => 'rgba(255, 165, 0, 0.75)')
+    .labelResolution(2)
+    .labelLabel(d => `
+        <div><b>IP: ${d.info.ip}</b></div>
+        <div>ORG: ${d.info.org}</div>`
+    )
+    (document.getElementById('globeViz'))
+
+globe.onZoom(({ lat, lng, altitude }) => {
+    globe.labelSize(d => .5*altitude);
+})
+globe.labelSize(d => {
+    let altitude = globe.pointOfView().altitude;
+    return .5*altitude;
+})
---
+++ main/views.py
@@ -0,0 +1,79 @@
+from django.shortcuts import render
+import subprocess
+import json
+import re
+import requests
+# Create your views here.
+
+
+
+IP_PATTERN = re.compile(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})')
+
+def get_terminal_line(line, line_num):
+    if line_num == 0:
+        line_type = "input"
+    else:
+        line_type = "output"
+    return json.dumps({"type": line_type, "msg": line})
+
+def get_ip_from_line(line, line_num):
+    if line_num != 0:
+        ip = IP_PATTERN.search(line)
+        if ip:
+            return ip[0]
+        return None
+    else:
+        return None
+def trace(request):
+    if request.method == 'POST':
+        website = request.POST
+        website = website['host']
+        hop_list = []
+        i_list = []
+        line_list = []
+        command = ["tracert", "-h", "30", website]    
+        with subprocess.Popen(command, stdout=subprocess.PIPE, bufsize=1, universal_newlines=True) as process:
+            for i, line in enumerate(process.stdout):
+                ip = get_ip_from_line(line, i)
+                terminal_line = get_terminal_line(line, i)
+                if ip:
+                    print(ip)
+                    print(terminal_line)
+                    token = "761eab6307df4b"
+                    ip_geolocation_data = requests.get(f"https://ipinfo.io/{ip}?token={token}").json()
+                    hop_list.append(ip_geolocation_data)
+                    geo_loc_msg = json.dumps({"type": "geo", "msg": ip_geolocation_data})
+                    i_list.append(geo_loc_msg)
+                    line_list.append(terminal_line)
+    
+
+    # my_list = json.dumps(i_list)
+    # dataDictionary = {
+    #     'hello': 'World',
+    #     'geeks': 'forgeeks',
+    #     'ABC': 123,
+    #     456: 'abc',
+    #     14000605: 1,
+    #     'list': ['geeks', 4, 'geeks'],
+    #     'dictionary': {'you': 'can', 'send': 'anything', 3: 1}
+    # }
+    # # dump data
+    # dataJSON = json.dumps(dataDictionary)
+    # test = {
+    #     "type": "geo",
+    #  "msg": {"ip": "142.250.71.36", "hostname": "maa03s35-in-f4.1e100.net", "city": "Chennai", "region": "Tamil Nadu", "country": "IN", "loc": "13.0878,80.2785", "org": "AS15169 Google LLC", "postal": "600001", "timezone": "Asia/Kolkata"}
+
+    
+    
+        data = json.dumps(hop_list)
+
+        context = {
+            'i_list' : i_list,
+            'line_list' : line_list,
+            'data' : data
+        }
+
+        return render(request, 'main/index.html', context)
+    
+    else :
+        return render(request, 'main/index.html')
